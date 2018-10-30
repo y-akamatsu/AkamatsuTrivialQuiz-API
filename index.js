112e417ba9eaa3be6ee6d41c7bcd05e2555f1a6d
y-akamatsu <ak1985tk@gmail.com>
2018-10-31 00:15:01 +0900

    index.ejs quiz.ejs修正

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
