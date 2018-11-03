4d0be0df2a5aa47be520c6afec99d57223927b35
y-akamatsu <ak1985tk@gmail.com>
2018-11-03 15:27:55 +0900

    APIClient.spec.jsの実装

---
+++ test/libs/APIClient.spec.js
@@ -0,0 +1,12 @@
+const assert = require('power-assert');
+const APIClient = require('../../libs/APIClient');
+
+describe('APIClientクラスのテスト', () => {
+  it('APIClient.fetchの挙動チェック', () => {
+    return APIClient
+      .fetch('https://opentdb.com/api.php?amount=10')
+      .then(json => {
+        assert.equal(json.url, true);
+      });
+  });
+});
