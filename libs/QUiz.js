e93940b6733d68aa1a05fdc56039e4a3da64653e
y-akamatsu <yoshimatsuakatoshi0202@gmail.com>
2018-11-11 02:44:26 +0900

    Quizクラスの実装

---
+++ libs/QUiz.js
@@ -0,0 +1,14 @@
+//クイズ1件1件のデータを管理するクラスをつくる。(ここではQuizクラスとする)
+//コンストラクタで受け取る値は`results` 1件1件のオブジェクト内容
+class Quiz{
+  constructor(category ,type, difficulty, question, correct_answer, incorrect_answers){
+  this.category = category;
+  this.type = type;
+  this.difficulty = difficulty;
+  this.question = question;
+  this.correct_ansewr = correct_answer;
+  this.incorrect_answers = incorrect_answers;
+  }
+}
+
+module.exports = Quiz;
---
+++ test/libs/Quiz.spec.js
@@ -0,0 +1,8 @@
+const assert = require('power-assert');
+const Quiz = require('../../libs/Quiz');
+
+describe('Quizクラスのテスト', () => {
+  it('1件1件のデータを管理する', () =>{
+    assert.equal()
+  });
+});
