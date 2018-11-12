ee68b977a8dfc97692ebbd2bb6b5f7661a2acd51
y-akamatsu <yoshimatsuakatoshi0202@gmail.com>
2018-11-13 00:06:06 +0900

    QUiz.js修正

--- libs/QUiz.js
+++ libs/QUiz.js
@@ -1,13 +1,13 @@
 //クイズ1件1件のデータを管理するクラスをつくる。(ここではQuizクラスとする)
 //コンストラクタで受け取る値は`results` 1件1件のオブジェクト内容
 class Quiz{
-  constructor(category, type, difficulty, question, coorect_answer, incorrect_answers){
+  constructor(category, type, difficulty, question, correctAnswer, incorrectAnswers){
   this.category = category;
   this.type = type;
   this.difficulty = difficulty;
   this.question = question;
-  this.correct_ansewr = correct_answer;
-  this.incorrect_answers = incorrect_answers;
+  this.correctAnsewr = correctAnswer;
+  this.incorrectAnswers = incorrectAnswers;
   }
 }
 
