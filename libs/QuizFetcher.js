d94d903806cf2e76f951d0d1375bd3b9889ce0d5
y-akamatsu <ak1985tk@gmail.com>
2018-11-15 22:58:43 +0900

    QuizFetcherクラスの実装2

--- libs/QuizFetcher.js
+++ libs/QuizFetcher.js
@@ -2,21 +2,20 @@ const APIClient = require('../../libs/APIClient');
 const Quiz = require('../../libs/Quiz');
 
 class QuizFetcher {
-  // APIClient.fetchメソッドを使ってクイズデータを取得
-  APIClient() {
-    fetch('https://opentdb.com/api.php?amount=10')
-      .then(response => {
-        return response.json();
+  static fetch() {
+    // 1. APIClient.fetchで問題データ取得
+    APIClient.fetch('https://opentdb.com/api.php?amount=10')
+      .then(json => {
+        return json();
       });
   }
-  //クイズデータをQuizクラスのインスタンスとしてデータを保持
-  Quiz() {
-    return [
-      this.category = category,
-      this.type = type,
-      this.difficulty = difficulty,
-      this.question = question,
-      this.correctAnswer = correctAnswer,
-      this.incorrectAnswers = incorrectAnswers];
-  }
+  // 2. 1で取得した問題データを使ってQuizクラスのインスタンス生成
+
 };
+
+// 3. 2で生成されるQuizクラスのインスタンスは複数(おそらく10個)あるので、それを１つの配列に格納する。
+// 4. 3で作ったQuizインスタンスが格納されている配列を呼び出し元に返す
+
+
+
+module.exports = QuizFetcher;
