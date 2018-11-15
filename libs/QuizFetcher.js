f7055b36d04760d6514ef67bda0c2754bb0d5c58
y-akamatsu <ak1985tk@gmail.com>
2018-11-15 23:51:12 +0900

    QuixFetcherクラスの実装3

--- libs/QuizFetcher.js
+++ libs/QuizFetcher.js
@@ -6,11 +6,21 @@ class QuizFetcher {
     // 1. APIClient.fetchで問題データ取得
     APIClient.fetch('https://opentdb.com/api.php?amount=10')
       .then(json => {
-        return json();
+        // `APIClient.spec.js`のテスト内容より `json.results` がクイズデータになる
+        return json.results;
       });
+    // json.resultsのデータを使ってQuizクラスのインスタンス生成
+    // (json.resultsは複数あるので、ここで複数のQuizクラスのインスタンスを生成)
+    // return Quizインスタンスの配列とすることで、呼び出し元はPromiseオブジェクトのthen経由でQuizインスタンスの配列を取得でき
+    Quiz(json.results){
+      return [this.category,
+      this.type,
+      this.difficulty,
+      this.question,
+      this.correctAnswer,
+      this.incorrectAnswers];
+    };
   }
-  // 2. 1で取得した問題データを使ってQuizクラスのインスタンス生成
-
 };
 
 // 3. 2で生成されるQuizクラスのインスタンスは複数(おそらく10個)あるので、それを１つの配列に格納する。
