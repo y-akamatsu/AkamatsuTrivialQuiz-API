4cf348284f63b70bdd7a5a28cf7e28baa1fd7d58
y-akamatsu <ak1985tk@gmail.com>
2018-11-20 00:34:19 +0900

    QuizFetcher.jsの実装とテストコード

--- libs/QuizFetcher.js
+++ libs/QuizFetcher.js
@@ -1,23 +1,36 @@
-const APIClient = require('../../libs/APIClient');
-const Quiz = require('../../libs/Quiz');
+const APIClient = require('./APIClient');
+const Quiz = require('./Quiz');
 
-class quizFetcher {
+class QuizFetcher {
   static fetch() {
     // 1. APIClient.fetchで問題データ取得
-    APIClient.fetch('https://opentdb.com/api.php?amount=10')
+    return APIClient
+      .fetch('https://opentdb.com/api.php?amount=10')
       .then(response => {
-        return response.json();
-        // `APIClient.spec.js`のテスト内容より `json.results` がクイズデータになる
-        // json.resultsのデータを使ってQuizクラスのインスタンス生成
-        const quiz = new Quiz('category', 'type', 'difficulty', 'question', 'correctAnswer', [0, 1, 2]);
+        //コールバック関数の引数のresponseオブジェクトでresultsの配列を受け取る
+        //変数resultsにresponse.resultsを代入
+        const results = response.results;
+        // 2. 1で取得した問題データを使ってQuizクラスのインスタンス生成
+        //quizInstanceListという空の配列を作成
+        const quizInstanceList = [];
+        //forEachでresultsの配列から1つづつ値を取得しコールバック関数の引数のquizDataに代入する
+        results.forEach(quizData => {
+          const quiz = new Quiz(
+            quizData.category,
+            quizData.type,
+            quizData.difficulty,
+            quizData.question,
+            quizData.correctAnswer,
+            quizData.incorrectAnswers
+          );
+          // 3. 2で生成されるQuizクラスのインスタンスは複数(おそらく10個)あるので、それを１つの配列に格納する。
+          //quizInstanceListの配列の要素に対して新たな配列を生成して戻り値を返す
+          quizInstanceList.map(quiz);
+        });
+        // 4. 3で作ったQuizインスタンスが格納されている配列を呼び出し元に返す
+        return quizInstanceList;
       });
-    // (json.resultsは複数あるので、ここで複数のQuizクラスのインスタンスを生成)
   }
 };
 
-// 3. 2で生成されるQuizクラスのインスタンスは複数(おそらく10個)あるので、それを１つの配列に格納する。
-// 4. 3で作ったQuizインスタンスが格納されている配列を呼び出し元に返す
-
-
-
-module.exports = quizFetcher;
+module.exports = QuizFetcher;
---
+++ test/libs/QuizFetcher.spec.js
@@ -0,0 +1,8 @@
+const assert = require('power-assert');
+const QuizFetcher = require('../../libs/QuizFetcher');
+
+describe('QuizFetcherクラスのテスト', () => {
+  it('Quizインスタンスの中身の確認', () => {
+    
+  });
+})
