e83ae09edf84a108dad0feb8ad50fba05cb1af0d
y-akamatsu <ak1985tk@gmail.com>
2018-12-02 10:18:50 +0900

    修正

--- test/libs/QuizFetcher.spec.js
+++ test/libs/QuizFetcher.spec.js
@@ -3,16 +3,11 @@ const QuizFetcher = require('../../libs/QuizFetcher');
 
 describe('QuizFetcherクラスのテスト', () => {
   it('QuizFetcher.fetchメソッドの実行結果の確認', () => {
-    // QuizFetcher.fetchはPromiseオブジェクトを返すため、returnをつけることで非同期のテストが出来る
-    // 1. QuizFetcher.fetchを実行
     return QuizFetcher
-      // 2. QuizFetcher.fetchを実行した際に、Promise経由で値を取得する。
       .fetch()
       .then(quizInstanceListMap => {
-        // 3. 2で取得した値の内容がQuizインスタンスの配列であるかどうか確認する。
         assert.equal(Array.isArray(quizInstanceListMap), true);
-        // 4. 各Quizインスタンスのプロパティに値が入っていることを確認する
-        assert.equal((quiz), quizInstanceListMap);
+        assert.equal(quizInstanceListMap);
       });
   });
 });
