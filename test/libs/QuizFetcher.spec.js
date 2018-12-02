33622599f8f17157d605250338d791d9e4aee263
y-akamatsu <ak1985tk@gmail.com>
2018-12-02 21:55:11 +0900

    値の確認

--- test/libs/QuizFetcher.spec.js
+++ test/libs/QuizFetcher.spec.js
@@ -3,10 +3,15 @@ const QuizFetcher = require('../../libs/QuizFetcher');
 
 describe('QuizFetcherクラスのテスト', () => {
   it('QuizFetcher.fetchメソッドの実行結果の確認', () => {
+        // 1. QuizFetcher.fetchを実行
     return QuizFetcher
       .fetch()
+        // 2. QuizFetcher.fetchを実行した際に、Promise経由で値を取得する。
       .then(quizInstanceListMap =>{
+        // 3. 2で取得した値の内容がQuizインスタンスの配列であるかどうか確認する。
         assert.equal(Array.isArray(quizInstanceListMap), true);
+        // 4. 各Quizインスタンスのプロパティに値が入っていることを確認する。
+        assert.equal(quizInstanceListMap,);
       });
   });
 });
