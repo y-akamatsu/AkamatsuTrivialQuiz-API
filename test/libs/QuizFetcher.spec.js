15c32321106df07bea1e41e7e10c106140bbe387
y-akamatsu <ak1985tk@gmail.com>
2018-12-09 12:06:05 +0900

    incoorectAnswersのテスト２

--- test/libs/QuizFetcher.spec.js
+++ test/libs/QuizFetcher.spec.js
@@ -18,7 +18,7 @@ describe('QuizFetcherクラスのテスト', () => {
           assert.equal(typeof quizData.question, 'string', 'プロパティに値が入っていません');
           assert.equal(typeof quizData.correctAnswer, 'string', 'プロパティに値が入っていません');
           assert.equal(Array.isArray(quizData.incorrectAnswers), true, 'プロパティに値が入っていません');
-          assert.equal(quizData.incorrectAnswers.length > 0, false, '配列に値が一つも入っていません');
+          assert.equal(quizData.incorrectAnswers.length > 0, true, '配列に値が一つも入っていません');
         });
       });
   });
