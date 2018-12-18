742244a4daeefb31539df2decbc69ad3597329a0
y-akamatsu <ak1985tk@gmail.com>
2018-12-18 21:21:26 +0900

    <%%>挙動確認

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-      res.render("index");
+  res.render("index");
 });
 
 app.get("/quiz", function (req, res) {
