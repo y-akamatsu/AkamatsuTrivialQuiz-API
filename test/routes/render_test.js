f1c476f29365081b2397c418eb35deb88e228e02
y-akamatsu <ak1985tk@gmail.com>
2019-01-05 21:54:46 +0900

    修正

--- test/routes/render_test.js
+++ test/routes/render_test.js
@@ -1,11 +1,11 @@
-// const request = require('supertest');
-// const app = require('../../index');
+const request = require('supertest');
+const app = require('../../index');
 
-// describe('GET/', () => {
-//   it('renderのルーティングの確認', (done) => {
-//     request(app)
-//       .get('/')
-//       .set('Accept', 'application/json')
-//       .expect(200, done)
-//   });
-// });
+describe('GET/', () => {
+  it('renderのルーティングの確認', (done) => {
+    request(app)
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect(200, done)
+  });
+});
