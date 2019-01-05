28f153cb9381dc10ca1c1e5330baee6602256c7c
y-akamatsu <ak1985tk@gmail.com>
2019-01-05 15:21:43 +0900

    テストの修正

--- test/routes/api_test.js
+++ test/routes/api_test.js
@@ -9,7 +9,17 @@ describe('GET/api/quiz', () => {
       .set('Accept', 'application/json')
       .expect(200)
       .then(response => {
+        assert.equal(Array.isArray(response.body), true, '配列ではありません');
         assert.equal(response.body.length, 10, 'データの数が合っていません');
+        response.body.forEach(quizData => {
+          assert.equal(typeof quizData.category, 'string', 'プロパティの型が正しくありません');
+          assert.equal(typeof quizData.type, 'string', 'プロパティの型が正しくありません');
+          assert.equal(typeof quizData.difficulty, 'string', 'プロパティの型が正しくありません');
+          assert.equal(typeof quizData.question, 'string', 'プロパティの型が正しくありません');
+          assert.equal(typeof quizData.correctAnswer, 'string', 'プロパティの型が正しくありません');
+          assert.equal(Array.isArray(quizData.incorrectAnswers), true, 'プロパティの型が配列になっていません');
+          assert.equal(quizData.incorrectAnswers.length > 0, true, 'プロパティの型に値が一つも入っていません');
+        });
       });
   });
 });
