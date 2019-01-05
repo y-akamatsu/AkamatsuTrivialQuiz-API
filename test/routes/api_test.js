6fe6a3ccd52bdb4c6c0b4f7b6998c9bd952e40f6
y-akamatsu <ak1985tk@gmail.com>
2019-01-05 12:37:19 +0900

    .thenとrequireの修正

--- test/routes/api_test.js
+++ test/routes/api_test.js
@@ -1,14 +1,15 @@
 const request = require('supertest');
-const app = require('../../routes/api');
+const app = require('../../index');
+const assert = require('power-assert');
 
 describe('GET/api/quiz', () => {
-  it('responds with json', () => {
+  it('Quizデータが10個返ってくるかのチェック', () => {
     return request(app)
       .get('/api/quiz')
       .set('Accept', 'application/json')
       .expect(200)
-      .then(quizInstanceListMap => {
-        assert(quizInstanceListMap.results, 10);
+      .then(response => {
+        assert.equal(response.body.length, 10, 'データの数が合っていません');
       });
   });
 });
