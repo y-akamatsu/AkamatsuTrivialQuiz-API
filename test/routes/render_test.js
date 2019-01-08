a93b72545e8137056d0866db1559f9165197c54a
y-akamatsu <ak1985tk@gmail.com>
2019-01-08 21:30:35 +0900

    リファクタリング2

--- test/routes/render_test.js
+++ test/routes/render_test.js
@@ -1,20 +1,29 @@
 const request = require('supertest');
 const app = require('../../index');
 
-function runRequestTest(URL) {
-  request(app)
-    .get(URL)
-    .expect(200)
+function runRequestTest(url) {
+  return new Promise(function (resolve) {
+    request(app)
+      .get(url)
+      .expect(200);
+    resolve(url);
+  });
 }
 
 describe('GET /', () => {
   it('indexファイルのrenderのルーティングの確認', () => {
-    return runRequestTest('/')
+    runRequestTest()
+      .then(url => {
+        assert.equal(url, '/');
+      });
   });
 });
 
 describe('GET /quiz', () => {
   it('quizファイルのrenderのルーティングの確認', () => {
-    return runRequestTest('/quiz')
+    return runRequestTest()
+      .then(url => {
+        assert.equal(url, '/quiz');
+      });
   });
 });
