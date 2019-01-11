8f7379d8024a03e499553e2cd5f00c837e97a66e
y-akamatsu <ak1985tk@gmail.com>
2019-01-11 09:43:36 +0900

    リファクタリング修正

--- test/routes/render_test.js
+++ test/routes/render_test.js
@@ -1,23 +1,22 @@
 const request = require('supertest');
 const app = require('../../index');
 
-function runRequestTest(url) {
-  return Promise.resolve(
-  request(app)
-  .get(url)
-  .expect(200)
-  );
-}
+//テストコードを関数化する
 
+function runRequestTest(url) {
+  return request(app)
+      .get(url)
+      .expect(200);
+}
 describe('GET /', () => {
   it('indexファイルのrenderのルーティングの確認', () => {
-    return runRequestTest('/')
+    return runRequestTest('/');
   });
 });
 
 describe('GET /quiz', () => {
   it('quizファイルのrenderのルーティングの確認', () => {
-    return runRequestTest('/quiz')
+    return runRequestTest('/quiz');
   });
 });
 
