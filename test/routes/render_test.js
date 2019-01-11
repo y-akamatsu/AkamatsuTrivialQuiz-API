e4a60391480d95b991b96a11c87dbc99ba9b13fc
y-akamatsu <ak1985tk@gmail.com>
2019-01-11 13:31:35 +0900

    異常系のテスト(404)のときのテスト実装

--- test/routes/render_test.js
+++ test/routes/render_test.js
@@ -3,20 +3,27 @@ const app = require('../../index');
 
 //テストコードを関数化する
 
-function runRequestTest(url) {
+function runRequestTest(url, statusCode) {
   return request(app)
-      .get(url)
-      .expect(200);
+    .get(url)
+    .expect(statusCode);
 }
+
 describe('GET /', () => {
   it('indexファイルのrenderのルーティングの確認', () => {
-    return runRequestTest('/');
+    return runRequestTest('/', 200);
   });
 });
 
 describe('GET /quiz', () => {
   it('quizファイルのrenderのルーティングの確認', () => {
-    return runRequestTest('/quiz');
+    return runRequestTest('/quiz', 200);
+  });
+});
+
+describe('GET /aaa', () => {
+  it('renderのルーティングのエラー確認', () => {
+    return runRequestTest('/aaa', 404);
   });
 });
 
